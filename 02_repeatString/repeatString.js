c68512064af2c4c675a2dc3d640623e2c70981a4
Simen Schikulski <schikulski@gmail.com>
2025-01-29 15:48:38 +0100

    Ferdig med alt opp til 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+        if (numberOfTimes < 0) {
+            return "ERROR";
+        }
+        let result = "";
+        for (let index = 0; index < numberOfTimes; index++) {
+            result = result + string;
+        }
+        return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeList) {
+
+    let resultArray = [];
+
+    for (const item of array) { // check each item in the array against the exclude list
+        for (const itemToRemove of removeList) {
+            if (!(item === itemToRemove)) {
+                console.log(`Item: ${item} is good. Pushing it to resultArray.`);
+                resultArray.push(item);
+            } else {
+                console.log(`Woops! item: ${item} is in the removeList, so not included.`);
+            }
+        }
+    }
+
+    return resultArray;
 };
 
 // Do not edit below this line
