ebce20969fc0f68604438c962321a775894f1acc
Simen Schikulski <schikulski@gmail.com>
2025-02-02 18:26:37 +0100

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+
+    // Handle ERRORs
+    if (startInt < 1 || endInt < 1 || !Number.isInteger(startInt) || !Number.isInteger(endInt)) return "ERROR";
+    
+    // Swap values
+    if (startInt > endInt) [startInt, endInt] = [endInt, startInt];
+
+    let finalSum = 0;
+
+    for (; startInt <= endInt; startInt++) {
+        finalSum += startInt;
+    }
+
+    return finalSum;
 
 };
 
