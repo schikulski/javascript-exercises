d02e836ebe48a1733249eb005afbc710624a3687
Simen Schikulski <schikulski@gmail.com>
2025-02-02 17:52:27 +0100

    Ferdig med 04 ;

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,51 @@
-const removeFromArray = function(array, ...removeList) {
+const removeFromArray = function(inputArray, ...arrayWithItemsToRemove) {
 
-    let resultArray = [];
 
-    for (const item of array) { // check each item in the array against the exclude list
-        for (const itemToRemove of removeList) {
-            if (!(item === itemToRemove)) {
-                console.log(`Item: ${item} is good. Pushing it to resultArray.`);
-                resultArray.push(item);
-            } else {
-                console.log(`Woops! item: ${item} is in the removeList, so not included.`);
-            }
-        }
-    }
+    // Versjon med for loops i to dimensjoner.
 
-    return resultArray;
+    // console.log(`--- \ninputArray is ${inputArray}, and array with values to remove is ${arrayWithItemsToRemove}`);
+    // for (let i = 0; i < inputArray.length; i++) {
+    //     let item = inputArray[i];
+    //     console.log(`--- \nNow checking ${item} against ${arrayWithItemsToRemove}. Index is now ${i}`);
+
+    //     for (let j = 0; j < arrayWithItemsToRemove.length; j++) {
+    //         let itemToRemove = arrayWithItemsToRemove[j];
+    //         console.log(`\t Checking if ${item} matches ${itemToRemove}`);
+    //         if (item === itemToRemove) {
+    //             console.log(`\t\t  Found ${item} to match ${itemToRemove}, so splicing index ${i}.`);
+    //             inputArray.splice(i, 1);
+    //             i = i - 1;
+    //             console.log(`\t\t  Since array is spliced, we move the i one step back. i is now ${i}`);
+    //             console.log(`\t\t  inputArray is currently ${inputArray}`);
+    //         } else {
+    //             console.log(`\t\t  No match for ${item} against ${itemToRemove}.`);
+    //         }
+    //     }
+
+    // }
+    // console.log(`--- \nResulting array is ${inputArray}`);
+    // return inputArray;
+
+    // Versjon med .includes
+    // let resultingArray = [];
+    // inputArray.forEach(item => {
+    //     if(!arrayWithItemsToRemove.includes(item)) {
+    //         resultingArray.push(item);
+    //     }
+    // });
+    // return resultingArray;
+    
+
+    // Versjon med .filter
+
+    // This works
+    // return inputArray.filter(item => !arrayWithItemsToRemove.includes(item));
+
+    // This works not
+    return inputArray.filter(item => {
+        return !arrayWithItemsToRemove.includes(item); // hvis funksjonen er innen {} så må vi si hva som skal returneres, i forhold til eksemplet over hvor shorthanden returnerer den eneste expression.
+    });
+
 };
 
 // Do not edit below this line
